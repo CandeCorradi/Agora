989e67076b842752c25cc247d3d37dd6ba273142
CandeCorradi <corradicande@gmail.com>
2025-08-21 18:58:56 -0300

    Añadir nuevos proyectos y datos climáticos
    Se han realizado cambios significativos en varios archivos del proyecto, incluyendo la adición de nuevos proyectos como "WebBlazor" y "TestAgora" en `Agora.sln`. En `Service.csproj`, se han añadido elementos de compilación y recursos embebidos, así como la creación de la interfaz `IGenericService` para operaciones CRUD en `IGenericService.cs`.
    
    Se ha implementado la lógica para obtener datos de una API en `GenericService.cs` y se han realizado mejoras en la interfaz de usuario con nuevos componentes y estilos en `NavMenu.razor`, `MainLayout.razor` e `index.html`. También se han agregado archivos de estilo como `favicon.png` y `bootstrap.min.css`.
    
    Además, se han añadido nuevos datos al archivo `weather.json`, que ahora contiene un arreglo de objetos con condiciones climáticas para varias fechas, enriqueciendo la información disponible. Por último, se han implementado pruebas unitarias en `UnitTestGenericService.cs` para verificar el funcionamiento del servicio genérico.

---
+++ Service/Services/Utils/ApiEndPoints.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services.Utils
+{
+
+        public static class ApiEndpoints
+        {
+            public static string Capacitacion { get; set; } = "capacitaciones";
+            public static string Usuario { get; set; } = "usuarios";
+            public static string Inscripcion { get; set; } = "inscripciones";
+            public static string TipoInscripcion { get; set; } = "tipos inscripciones";
+            public static string TipoInscripcionCapacitacion { get; set; } = "tipos inscripciones capacitaciones";
+           
+            public static string GetEndpoint(string name)
+            {
+                return name switch
+                {
+                    nameof(Capacitacion) => Capacitacion,
+                    nameof(Usuario) => Usuario,
+                    nameof(Inscripcion) => Inscripcion,
+                    nameof(TipoInscripcion) => TipoInscripcion,
+                    nameof(TipoInscripcionCapacitacion) => TipoInscripcionCapacitacion,
+                    _ => throw new ArgumentException($"Endpoint '{name}' no está definido.")
+                };
+            }
+        }
+    
+}
---
+++ TestAgora/UnitTestGenericService.cs
@@ -0,0 +1,26 @@
+using Service.Models;
+using Service.Services;
+
+namespace TestAgora
+{
+    public class UnitTestGenericService
+    {
+        [Fact]
+        public async void TestGetAll()
+        {
+            // Arrange
+            var service = new GenericService<Usuario>();
+            // Act
+            var result = await service.GetAllAsync(null);
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<Usuario>>(result);
+            Assert.True(result.Count > 0, "La lista de capacitaciones no debe estar vacía.");
+            foreach (var item in result)
+            {
+                //imprimimos las capacitaciones
+                Console.WriteLine($"Id: {item.Id}, Nombre: {item.Nombre}");
+            }
+        }
+    }
+}
